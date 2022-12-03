04dcde549ac0ea4e1b82ed5887440763e668959f
Mikael Sernheim <mikael.sernheim@getswish.se>
2022-12-03 23:57:23 +0100

    Added input files

--- src/Utils.kt
+++ src/Utils.kt
@@ -5,7 +5,7 @@ import java.security.MessageDigest
 /**
  * Reads lines from the given input txt file.
  */
-fun readInput(name: String) = File("src", "$name.txt")
+fun readInput(name: String) = File("src/input", "$name.txt")
     .readLines()
 
 /**
